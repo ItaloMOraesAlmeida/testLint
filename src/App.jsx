d4f850aa1af803091b19646111abeca7a68e0b23
Italo Moraes <italomoraesalmeida@hotmail.com>
2021-09-22 14:40:54 -0300

    :rocket:(refactor): test
    test new branch pull request

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello New World
-      <h1>New line in project 1</h1>
-    </div>
-  );
+  return <div className="App">Hello New World</div>;
 }
 
 export default App;
